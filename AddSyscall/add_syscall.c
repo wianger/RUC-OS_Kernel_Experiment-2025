a44f16954f4f0fe522b88ff786c0f8beb1add427
wiang <2692807124@qq.com>
2025-11-14 10:41:09 +0000

    feat: add add_syscall

--- AddSyscall/add_syscall.c
+++ AddSyscall/add_syscall.c
@@ -1,62 +1,57 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/unistd.h>
-#include <linux/time.h>
-#include <linux/sched.h>
 #include <linux/kallsyms.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/time.h>
+#include <linux/unistd.h>
 
-#define __NR_syscall 335    // 保持原系统调用号
+#define __NR_syscall 335 // 保持原系统调用号
 unsigned long *sys_call_table;
 
 unsigned int clear_and_return_cr0(void);
 void setback_cr0(unsigned int val);
 static int sys_mycall(void); // 保持原函数名
 
-int orig_cr0;               // 保持原变量名
+int orig_cr0;                       // 保持原变量名
 static int (*anything_saved)(void); // 保持原指针类型
 
 /* 完全保留原CR0操作方式 */
-unsigned int clear_and_return_cr0(void)
-{
-    unsigned int cr0 = 0;
-    unsigned int ret;
-    asm volatile ("movq %%cr0, %%rax" : "=a"(cr0));
-    ret = cr0;
-    cr0 &= 0xfffeffff;
-    asm volatile ("movq %%rax, %%cr0" :: "a"(cr0));
-    return ret;
+unsigned int clear_and_return_cr0(void) {
+  unsigned int cr0 = 0;
+  unsigned int ret;
+  asm volatile("movq %%cr0, %%rax" : "=a"(cr0));
+  ret = cr0;
+  cr0 &= 0xfffeffff;
+  asm volatile("movq %%rax, %%cr0" ::"a"(cr0));
+  return ret;
 }
 
-void setback_cr0(unsigned int val)
-{
-    asm volatile ("movq %%rax, %%cr0" :: "a"(val));
+void setback_cr0(unsigned int val) {
+  asm volatile("movq %%rax, %%cr0" ::"a"(val));
 }
 
-static int sys_mycall(void)
-{
-    return task_tgid_vnr(current);
+static int sys_mycall(void) { return task_tgid_vnr(current); }
+
+static int __init init_addsyscall(void) {
+  // TODO: get sys_call_table
+  sys_call_table = (unsigned long *)kallsyms_lookup_name("sys_call_table");
+
+  anything_saved = (int (*)(void))(sys_call_table[__NR_syscall]);
+
+  orig_cr0 = clear_and_return_cr0();
+  // TODO: modify sys_call_table item
+  sys_call_table[__NR_syscall] = (unsigned long)sys_mycall;
+
+  setback_cr0(orig_cr0);
+
+  return 0;
 }
 
-static int __init init_addsyscall(void)
-{
-    // TODO: get sys_call_table 
-    
-    anything_saved = (int (*)(void))(sys_call_table[__NR_syscall]);
-    
-    orig_cr0 = clear_and_return_cr0();
-    // TODO: modify sys_call_table item
-
-    setback_cr0(orig_cr0);
-    
-    return 0;
-}
-
-static void __exit exit_addsyscall(void)
-{
-    orig_cr0 = clear_and_return_cr0();
-    sys_call_table[__NR_syscall] = (unsigned long)anything_saved;
-    setback_cr0(orig_cr0);
+static void __exit exit_addsyscall(void) {
+  orig_cr0 = clear_and_return_cr0();
+  sys_call_table[__NR_syscall] = (unsigned long)anything_saved;
+  setback_cr0(orig_cr0);
 }
 
 module_init(init_addsyscall);
--- AddSyscall/test.c
+++ AddSyscall/test.c
@@ -5,10 +5,10 @@
 #define MY_SYSCALL_NR 335
 
 int main() {
-    long pid = syscall(MY_SYSCALL_NR);
-    
-    printf("Syscall returned PID: %ld\n", pid);
-    printf("Compare with getpid(): %d\n", getpid());
-    
-    return 0;
+  long pid = syscall(MY_SYSCALL_NR);
+
+  printf("Syscall returned PID: %ld\n", pid);
+  printf("Compare with getpid(): %d\n", getpid());
+
+  return 0;
 }
